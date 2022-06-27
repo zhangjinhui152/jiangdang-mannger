0adfd83f66cd286c0540bb3083199b93df673f0a
zhangjinhi <zhangjinhui152@gmail.com>
2022-06-27 19:40:08 +0800

    改为hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from "../views/Home.vue";
 
 const routes = [
+    
     {
         path: "/",
         redirect: "/dashboard",
@@ -228,7 +229,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(),
     routes,
 });
 
